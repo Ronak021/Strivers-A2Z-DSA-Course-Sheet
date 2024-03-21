3b554f1caf31e1fd12df6fd660f0c6ded0372b93
Ronak Chauhan <121048729+Ronak021@users.noreply.github.com>
2024-03-21 23:01:45 +0530

    Update 1. Largest Element in an Array.cpp

--- Arrays/Easy/1. Largest Element in an Array.cpp
+++ Arrays/Easy/1. Largest Element in an Array.cpp
@@ -1,7 +1,15 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h> 
+int largestElement(vector<int> &arr, int n) {
 
-int main()
-{
-    
+    int max=arr[0];
+
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i]>max) max=arr[i];
+    }
+
+    return max;
 }
+
+
+// Complexity -> O(n)
